42d4224c101b0ddfdda0c8d7c5931f15eb4943a8
Kim Taemin <kimtaemin0203@gmail.com>
2025-07-23 16:58:36 +0900

    정보 입력

--- examples/data_channel/sportmode/sportmode.py
+++ examples/data_channel/sportmode/sportmode.py
@@ -11,9 +11,9 @@ logging.basicConfig(level=logging.FATAL)
 async def main():
     try:
         # Choose a connection method (uncomment the correct one)
-        conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip="192.168.8.181")
+        # zxzxzconn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip="192.168.8.181")
         # conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, serialNumber="B42D2000XXXXXXXX")
-        # conn = Go2WebRTCConnection(WebRTCConnectionMethod.Remote, serialNumber="B42D2000XXXXXXXX", username="email@gmail.com", password="pass")
+        conn = Go2WebRTCConnection(WebRTCConnectionMethod.Remote, serialNumber="B42D4000O358LD01", username="mrt2020@daum.net", password="dodan1004~")
         # conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalAP)
 
         # Connect to the WebRTC service.
